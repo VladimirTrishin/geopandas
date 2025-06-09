6c725dddfa2f4a2ab9e04728fe0fa44021c71d81
Vladimir Trishin <v.trishin@inforion.ru>
2025-06-09 13:03:13 +0300

    pass search label to search plugin

--- geopandas/explore.py
+++ geopandas/explore.py
@@ -671,7 +671,7 @@ def _explore(
     if search_label:
         try:
             import folium.plugins
-            folium.plugins.Search(layer=data_layer, **search_kwds).add_to(m)
+            folium.plugins.Search(layer=data_layer, search_label=search_label, **search_kwds).add_to(m)
         except ImportError:
             raise ValueError("Tre folium.plugins is required for search")
 
